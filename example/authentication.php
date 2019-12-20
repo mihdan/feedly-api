6d68343ef76de452678c9706b183823a7e62d0a4
Mikhail Kobzarev <mihdan@yandex.ru>
2019-12-20 12:59:00 +0300

    Update client secret for example script

--- example/authentication.php
+++ example/authentication.php
@@ -1,4 +1,5 @@
 <?php
+namespace feedly;
 
 require dirname(__FILE__) . "/../vendor/autoload.php";
 
@@ -6,34 +7,33 @@ require dirname(__FILE__) . "/../vendor/autoload.php";
  * Read this before set your callback url while in Sandbox mode
  * @see https://groups.google.com/forum/#!topic/feedly-cloud/vSo0DuShvDg/discussion
  */
-$feedly = new feedly\Feedly(new feedly\Mode\SandBoxMode(), new feedly\AccessTokenStorage\AccessTokenSessionStorage());
+$feedly = new Feedly( new Mode\SandBoxMode(), new AccessTokenStorage\AccessTokenSessionStorage() );
 
-$loginUrl = $feedly->getLoginUrl("sandbox", "http://localhost");
-
-if (isset($_GET['code'])) {
+if ( isset( $_GET['code'] ) ) {
 
     /**
      * Response will contain the Access Token and Refresh Token
      */
     $tokens = $feedly->getTokens(
         "sandbox",
-        "JSSBD6FZT72058P51XEG",
+        "nZmS4bqxgRQkdPks",
         $_GET['code'],
         "http://localhost/"
     );
 
     /**
-     * You must update Client's Secret(YDRYI5E8OP2JKXYSDW79 ) once in a while
+     * You must update Client's Secret(nZmS4bqxgRQkdPks ) once in a while
      * @see https://groups.google.com/forum/#!topic/feedly-cloud/a_cGSAzv8bY
      */
 
     echo $tokens;
 }
 
-if (!isset($_SESSION['feedly_access_token'])) {
+if ( ! isset( $_SESSION['feedly_access_token'] ) ) {
     /**
      * After redirection replace "localhost" with your domain
      * keeping the Auth Code GET param
      */
+    $loginUrl = $feedly->getLoginUrl( 'sandbox', 'http://localhost' );
     echo "<a href=\"" . $loginUrl . "\">Authenticate using Feedly</a>";
 }
